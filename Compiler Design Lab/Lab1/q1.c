36d7bb0518d0b48fb936dead93772e949539bd2f
harshulmalhotra18 <101328704+harshulmalhotra18@users.noreply.github.com>
2023-07-31 11:03:12 +0530

    Add files via upload

---
+++ Compiler Design Lab/Lab1/q1.c
@@ -0,0 +1,26 @@
+//To count the number of lines and characters in a file.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main(){
+	int countline=0,countc=0;
+	char c;FILE *fptr;
+	fptr= fopen("input.txt","r");
+	if(!fptr){
+		printf("Cannot open file\n");
+		exit(0);
+	}
+	c=getc(fptr);
+	while(c!=EOF){
+		if(c=='\n') countline++;
+		
+		else countc++;
+
+		c=getc(fptr);
+	}
+	if(countc >0) countline++;
+	printf("Total number of lines and characters is %d and %d respectively\n",countline,countc);
+	return 0;
+}
